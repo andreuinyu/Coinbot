100b237cba11492d7c0eb733a311a13dcf167a68
Andreu Punsola <andreu4ps@gmail.com>
2017-11-12 00:52:33 +0100

    prou des

--- main.py
+++ main.py
@@ -2,28 +2,36 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryH
 from telegram import ReplyKeyboardMarkup, ReplyMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 from coinmarketcap import Market
 
-currencyinfo = False;
-conversion = False;
+currencyinfo = False
+conversionusd = False
+waitingamount = False
+waitingnumcurrencies = False
+rate = [None, 0]
+
 
 def start(bot, update):
     keyboard = [[InlineKeyboardButton("/market", callback_data='1'),
-                 InlineKeyboardButton("Option 2", callback_data='2')],
-                [InlineKeyboardButton("Option 3", callback_data='3')]]
+                 InlineKeyboardButton("/graph", callback_data='2')],
+
+                [InlineKeyboardButton("/converttousd", callback_data='3')]]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
-
     update.message.reply_text('Please choose:', reply_markup=reply_markup)
+
 
 def help(bot, update):
     update.message.reply_text('Need help? Fuck yourself')
+
 
 def currencyInfo(bot, update):
     global currencyinfo
     currencyinfo = True
     chooseCurrency(bot, update)
 
-def Convert(bot, update):
-    convert = True
+
+def ConvertToUSD(bot, update):
+    global conversionusd
+    conversionusd = True
     chooseCurrency(bot, update)
 
 
@@ -36,41 +44,67 @@ def chooseCurrency(bot, update):
         kb.append([])
         for col in range(3):
             kb[fila].append(KeyboardButton(text=array[i]["symbol"]))
-            i+=1
-    bot.sendMessage(update.message.chat.id, "What currency do you want info on?",
+            i += 1
+    bot.sendMessage(update.message.chat.id, "Choose currency:",
         reply_markup=ReplyKeyboardMarkup(
             keyboard=kb
         )
     )
 
+
 def marketInfo(bot, update):
-    out = "Summary of the top cryptocurrencies by market cap: \n"
-    coinmarketcap = Market()
-    array = coinmarketcap.ticker(limit=10)
-    for moneda in array:
-        out +='\n' + moneda['name'] +' ('+ moneda['symbol'] +')' +' = $' + moneda['price_usd']
-    update.message.reply_text(out)
+    global waitingnumcurrencies
+    waitingnumcurrencies = True
+    update.message.reply_text("Get me the top")
+
 
 def answerer(bot, update):
     text = update.message.text
+
     global currencyinfo
+    global conversionusd
+    global rate
+    global waitingamount
+    global waitingnumcurrencies
+
     if currencyinfo:
         coinmarketcap = Market()
         info = ""
         array = coinmarketcap.ticker(limit=6)
         for moneda in array:
             if moneda["symbol"] == text:
-                info = "Name: "+ moneda["name"]+' ('+ moneda['symbol'] +')' +"\nSupply: "
-                info+= moneda["available_supply"] + " of " + moneda["max_supply"] + text + "\n"
-                info+= "Value change 24h: " + moneda["percent_change_24h"] + "%"
-                info+= "\nValue change 7 day: " + moneda["percent_change_7d"] + "%"
+                info = "Name: " + moneda["name"] + ' (' + moneda['symbol'] + ')' + "\nSupply: "
+                info += moneda["available_supply"] + " of " + moneda["max_supply"] + text + "\n"
+                info += "Value change 24h: " + moneda["percent_change_24h"] + "%"
+                info += "\nValue change 7 day: " + moneda["percent_change_7d"] + "%"
                 update.message.reply_text(info)
                 break
-        currencyinfo = False;
+        currencyinfo = False
 
+    elif conversionusd:
+        conversionusd = False
+        coinmarketcap = Market()
+        array = coinmarketcap.ticker(limit=6)
+        for moneda in array:
+            if text == moneda["symbol"]:
+                rate[0] = text
+                rate[1] = float(moneda["price_usd"])
+                waitingamount = True
+                break
+        update.message.reply_text("Enter amount of " + rate[0])
 
-    elif convert:
-        pass
+    elif waitingamount:
+        waitingamount = False
+        update.message.reply_text(text + " " + rate[0] + " = $" + str(float(text)*rate[1]))
+
+    elif waitingnumcurrencies:
+        waitingnumcurrencies = False
+        out = "Summary of the top {} cryptocurrencies by market cap: \n".format(text)
+        coinmarketcap = Market()
+        array = coinmarketcap.ticker(limit=int(text))
+        for moneda in array:
+            out += '\n' + moneda['name'] + ' (' + moneda['symbol'] + ')' + ' = $' + moneda['price_usd']
+        update.message.reply_text(out)
 
 
 def graph(bot,update):
@@ -79,10 +113,12 @@ def graph(bot,update):
     #update.message.reply_text(img)
     bot.sendPhoto(update.message.chat.id, img)
 
+
 def button(bot, update):
     query = update.callback_query
     if query.data == "1":
         return marketInfo(bot, update)
+
 
 def main():
 
@@ -100,6 +136,7 @@ def main():
     bot.add_handler(CommandHandler("help", help))
     bot.add_handler(CommandHandler("graph", graph))
     bot.add_handler(CommandHandler("currency", currencyInfo))
+    bot.add_handler(CommandHandler("converttousd", ConvertToUSD))
     #   Missatges: Filtrar els de text
     bot.add_handler(MessageHandler(Filters.text, answerer))
     #   Botons
@@ -107,6 +144,7 @@ def main():
 
     botUpdater.start_polling()
     botUpdater.idle()
+
 
 if __name__ == "__main__":
     main()
