1c43e3acf414844be2351ce4af8fdef0f7b4ed71
Andreu Punsola <andreu4ps@gmail.com>
2017-11-11 22:05:19 +0100

    des*10^-15

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram import ParseMode
-import coinbase as cb
-
-
-processingsend = False
-waitingemail = False
-waitingamount = False
-botUpdater = None
-
 
 def start(bot, update):
     update.message.reply_text("sanx marika")
@@ -18,51 +9,15 @@ def help(bot, update):
 
 
 def send(bot, update):
-    global processingsend
-    global waitingemail
-    global waitingamount
-    processingsend = True
-    waitingemail = True
-    waitingamount = True
     update.message.reply_text('Send me the email of the receiver')
 
 
 def answerer(bot, update):
+    update.message.reply_text("in reader")
 
-    global processingsend
-    global waitingemail
-    global waitingamount
-    text = update.message.text
-    #update.message.reply_text("in reader")
-    #print(processingsend)
-
-    if processingsend and waitingemail:
-        email = text
-        waitingemail = False
-        update.message.reply_text("got email, enter amount")
-    elif processingsend == True and waitingamount == True:
-        amount = text
-        update.message.reply_text("got amount")
-        waitingamount = False
-        processingsend = False
-
-        """
-        response = cb.send_money('user@example.com', '2')
-        print(response['success'])
-        # True
-        print(response['transaction']['status'])
-        # 'pending'
-        print(response['transaction']['id'])
-        # '518d8567ed3ddcd4fd000034'
-        """
 
 
 def main():
-    global token
-    global botUpdater
-    processingsend = False
-    waitingemail = False
-    waitingamount = False
 
     with open('TelegramToken.txt', 'r') as tokentxt:
         # Obtenir d'un arxiu txt el token únic del bot en qüestió
