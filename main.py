39e8862f15be56e547e2ff4977e7e675b48c4d55
Andreu Punsola <andreu4ps@gmail.com>
2017-11-11 13:58:01 +0100

    initial commit

--- main.py
+++ main.py
@@ -26,12 +26,14 @@ def reverse(s):
 def main():
     coinbase_oauth = cb.CoinbaseOAuth("bf03ac56dd01694ba744831afdbcc94abbc9f36d804c59b5904f4549be2e4047",
                                       "38bc50fcef5a87f9703fcc5939c75edbd00b70244fe1d34ddd40a5b05b2f40db",
-                                      "http://www.t.me/CoBase_bot")
+                                      "https://www.t.me/CoBase_bot")
+    authurl = coinbase_oauth.create_authorize_url()
+    authurl= authurl[0:-6] + "&scope=balance+addresses+user+transactions"
 
-    print(coinbase_oauth.create_authorize_url())
+    print(authurl)
 
     with open('TelegramToken.txt', 'r') as tokentxt:
-        #Obtenir d'un arxiu txt el token únic del bot en qüestió
+        # Obtenir d'un arxiu txt el token únic del bot en qüestió
         token = tokentxt.readline().strip()
     botUpdater = Updater(token)
     bot = botUpdater.dispatcher
