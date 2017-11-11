0f3063a293ff4ea3c0447b67f9a74f3cfbd1b3a8
Andreu Punsola <andreu4ps@gmail.com>
2017-11-11 15:02:12 +0100

    des

--- main.py
+++ main.py
@@ -1,64 +1,67 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ParseMode
 import coinbase as cb
-
 
 processingsend = False
 waitingemail = False
 waitingamount = False
 
+
 def start(bot, update):
-    update.message.reply_text("sanx marika")
+    coinbase_oauth = cb.CoinbaseOAuth("bf03ac56dd01694ba744831afdbcc94abbc9f36d804c59b5904f4549be2e4047",
+                                      "38bc50fcef5a87f9703fcc5939c75edbd00b70244fe1d34ddd40a5b05b2f40db",
+                                      "https://www.t.me/CoBase_bot")
+    authurl = coinbase_oauth.create_authorize_url()
+    authurl = authurl[0:-6] + "&scope=balance+addresses+user+transactions"
+    msg = "[Allow access:]({})".format(authurl)
+    update.message.reply_text(msg, parseMode=ParseMode.MARKDOWN)
 
 
 def help(bot, update):
     update.message.reply_text('Need help? Fuck yourself')
 
+
 def send(bot, update):
-	global processingsend
-	global waitingemail
-	global waitingamount
-	processingsend = True
-	waitingemail = True
-	waitingamount = True
-	update.message.reply_text('enter email of the receiver')
+    global processingsend
+    global waitingemail
+    global waitingamount
+    processingsend = True
+    waitingemail = True
+    waitingamount = True
+    update.message.reply_text('Send me the email of the receiver')
+
 
 def answerer(bot, update):
-	global processingsend
-	global waitingemail
-	global waitingamount
-	text = update.message.text
-	#update.message.reply_text("in reader")	
-	#print(processingsend)
-	
-	if processingsend  and waitingemail  :
-		email = text
-		waitingemail = False
-		update.message.reply_text("got emai, enter amount")	
-	elif processingsend == True and waitingamount == True :
-		amount = text
-		update.message.reply_text("got amount")	
-		waitingamount = False
-		response = coinbase.send_money('user@example.com', '2')
-		processingsend = False
-		print(response['success'])
-		# True
-		print(response['transaction']['status'])
-		# 'pending'
-		print(response['transaction']['id'])
-		# '518d8567ed3ddcd4fd000034'
-	
-	#update.message.reply_text(reverse(text))
 
-	
+    global processingsend
+    global waitingemail
+    global waitingamount
+    text = update.message.text
+    #update.message.reply_text("in reader")
+    #print(processingsend)
 
+    if processingsend  and waitingemail  :
+        email = text
+        waitingemail = False
+        update.message.reply_text("got email, enter amount")
+    elif processingsend == True and waitingamount == True:
+        amount = text
+        update.message.reply_text("got amount")
+        waitingamount = False
+        processingsend = False
+
+        """
+        response = cb.send_money('user@example.com', '2')
+        print(response['success'])
+        # True
+        print(response['transaction']['status'])
+        # 'pending'
+        print(response['transaction']['id'])
+        # '518d8567ed3ddcd4fd000034'
+        """
 
 def main():
-    coinbase_oauth = cb.CoinbaseOAuth("bf03ac56dd01694ba744831afdbcc94abbc9f36d804c59b5904f4549be2e4047",
-                                      "38bc50fcef5a87f9703fcc5939c75edbd00b70244fe1d34ddd40a5b05b2f40db",
-                                      "https://www.t.me/CoBase_bot")
-    authurl = coinbase_oauth.create_authorize_url()
-    authurl= authurl[0:-6] + "&scope=balance+addresses+user+transactions"
-	
+
     processingsend = False
     waitingemail = False
     waitingamount = False
@@ -69,8 +72,6 @@ def main():
         token = tokentxt.readline().strip()
     botUpdater = Updater(token)
     bot = botUpdater.dispatcher
-
-
 
     # Events
     #   Commandos: comencen amb /
