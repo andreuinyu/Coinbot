95cd457a0c4643c274929820141c9d18c63948f1
Andreu Punsola <andreu4ps@gmail.com>
2017-11-12 11:05:43 +0100

    desills

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ conversion = False
 conversionEur = -1
 waitingamount = False
 waitingnumcurrencies = False
+waitingtarget = False
 rate = [None, 0]
 
 
@@ -24,7 +25,7 @@ def help(bot, update):
           "*Graph* \n /graph - Last 6 months of the bitcon price in USD\n" \
           "*Currency*\n /currency - Given a code of a cryptocurrency, displays its name, the existing supply and the " \
           "daily and weekly change in value\n" \
-          "*Convert* \n /convert - get the conversion from cryptocurrency of your choice to Euro or US Dollar"
+          "*Convert* \n /convert - get the conversion from cryptocurrency of your choice to Euros, US Dollars, Swiss Francs or Pounds"
     bot.sendMessage(update.message.chat.id, msg, parse_mode="Markdown")
 
 
@@ -35,17 +36,21 @@ def currencyInfo(bot, update):
 
 
 def Convert(bot, update):
-    global conversion
-    conversion = True
+    global waitingtarget
+    global waitingamount
+    waitingtarget = True
+    waitingamount = True
     chooseConversion(bot, update)
 
 
 def chooseConversion(bot, update):
-    kb = [[KeyboardButton(text="EUR")],[KeyboardButton(text="USD")]]
-    bot.sendMessage(update.message.chat.id,"Choose EUR or USD:",
-    reply_markup=ReplyKeyboardMarkup(keyboard=kb,one_time_keyboard=True)
+    kb = [[KeyboardButton(text="USD"), KeyboardButton(text="EUR")],
+          [KeyboardButton(text="CHF"), KeyboardButton(text="GBP")]]
+    bot.sendMessage(
+        update.message.chat.id,
+        "Choose target real currency:",
+        reply_markup=ReplyKeyboardMarkup(keyboard=kb, one_time_keyboard=True)
     )
-
 
 
 def chooseCurrency(bot, update):
@@ -60,7 +65,7 @@ def chooseCurrency(bot, update):
             i += 1
     bot.sendMessage(
         update.message.chat.id,
-        "Choose or type the desired crytocurrency symbol:",
+        "Choose or type the desired cryptocurrency symbol:",
         reply_markup=ReplyKeyboardMarkup(
             keyboard=kb,
             one_time_keyboard=True
@@ -83,6 +88,7 @@ def answerer(bot, update):
     global rate
     global waitingamount
     global waitingnumcurrencies
+    global waitingtarget
 
     if currencyinfo:
         found = False
@@ -91,8 +97,8 @@ def answerer(bot, update):
         for moneda in array:
             if moneda["symbol"] == text:
                 found = True
-                info = "Name: " + moneda["name"] + ' (' + moneda['symbol'] + ')'+"\nRanked: "+ moneda['rank']+ "\nSupply: "
-                info += moneda["available_supply"] + " of "
+                info = "Name: " + moneda["name"] + ' (' + moneda['symbol'] + ')'+"\nRanked: "
+                info += moneda['rank']+ "\nSupply: " + moneda["available_supply"] + " of "
                 if moneda["max_supply"] is None:
                     info += "unknown \n"
                 else:
@@ -103,37 +109,59 @@ def answerer(bot, update):
                 break
         if not found:
             update.message.reply_text("Cryptocurrency not found, choose another currency")
-        else :
+        else:
             currencyinfo = False
 
-    elif conversion and conversionEur == -1:
+    elif waitingtarget and conversionEur == -1:
+        waitingtarget = False
         if text == 'USD':
             conversionEur = 0
-        else:
+        elif text == "EUR":
             conversionEur = 1
+        elif text == "CHF":
+            conversionEur = 2
+        elif text == "GBP":
+            conversionEur = 3
+        conversion = True
         chooseCurrency(bot, update)
 
     elif conversion and conversionEur != -1:
         conversion = False
         coinmarketcap = Market()
-        array = coinmarketcap.ticker(limit=100, convert = 'EUR')
+        if conversionEur == 1:
+            array = coinmarketcap.ticker(limit=100, convert='EUR')
+        elif conversionEur == 2:
+            array = coinmarketcap.ticker(limit=100, convert='CHF')
+        elif conversionEur == 3:
+            array = coinmarketcap.ticker(limit=100, convert='GBP')
+        else:
+            array = coinmarketcap.ticker(limit=100, convert="USD")
         for moneda in array:
             if text == moneda["symbol"]:
                 rate[0] = text
-                if conversionEur == 0:
-                    rate[1] = float(moneda["price_usd"])
-                else:
+                if conversionEur == 1:
                     rate[1] = float(moneda["price_eur"])
+                elif conversionEur == 2:
+                    rate[1] = float(moneda["price_chf"])
+                elif conversionEur == 3:
+                    rate[1] = float(moneda["price_gbp"])
+                else:
+                    rate[1] = float(moneda["price_usd"])
                 waitingamount = True
                 break
         update.message.reply_text("Enter amount of " + rate[0])
 
     elif waitingamount:
         waitingamount = False
+        preu = "{:.2f}".format(float(text)*rate[1])
         if conversionEur == 0:
-            update.message.reply_text(text + " " + rate[0] + " = $" + str(float(text)*rate[1]))
-        else:
-            update.message.reply_text(text + " " + rate[0] + " = €" + str(float(text)*rate[1]))
+            update.message.reply_text(text + " " + rate[0] + " = $" + preu)
+        elif conversionEur == 1:
+            update.message.reply_text(text + " " + rate[0] + " = " + preu + "€")
+        elif conversionEur == 2:
+            update.message.reply_text(text + " " + rate[0] + " = " + preu + "Fr.")
+        elif conversionEur == 3:
+            update.message.reply_text(text + " " + rate[0] + " = £" + preu)
         conversionEur = -1
 
     elif waitingnumcurrencies:
@@ -149,18 +177,9 @@ def answerer(bot, update):
             update.message.reply_text("Only integers")
 
 
-def graph(bot,update):
-    #bitcoin usd
+def graph(bot, update):
     img = "https://bitcoincharts.com/charts/chart.png?width=940&m=bitmarketEUR&SubmitButton=Draw&r=180&i=&c=0&s=&e=&Prev=&Next=&t=S&b=&a1=&m1=10&a2=&m2=25&x=0&i1=&i2=&i3=&i4=&v=1&cv=0&ps=0&l=0&p=0&"
-    #update.message.reply_text(img)
     bot.sendPhoto(update.message.chat.id, img)
-
-
-def button(bot, update):
-    query = update.callback_query
-    if query.data == "1":
-        print("/market")
-        marketInfo(bot, update)
 
 
 def main():
@@ -182,8 +201,6 @@ def main():
     bot.add_handler(CommandHandler("convert", Convert))
     #   Missatges: Filtrar els de text
     bot.add_handler(MessageHandler(Filters.text, answerer))
-    #   Botons
-    bot.add_handler(CallbackQueryHandler(button))
 
     botUpdater.start_polling()
     botUpdater.idle()
